313ed9fe0652b32e56f5721ddf2ac82d59ecf8c2
Marsolo1 <martialmarzloff@gmail.com>
2023-11-10 17:32:30 +0100

    Add functions to download and extract datasets

--- download_resources.py
+++ download_resources.py
@@ -1,7 +1,11 @@
 import os
 import requests
 
-resources_folder: str = "resources/"
+import gdown
+import zipfile
+
+resources_folder: str = "resources"
+
 def download_and_extract_dataset():
     url = 'https://veekun.com/static/pokedex/downloads/pokemon-sugimori.tar.gz'
     save_dir = 'resources'
@@ -29,5 +33,29 @@ def download_and_extract_dataset():
     os.system(bash_command)
     print('done')
 
+
+
+def down_drive(url:str, dest:str):
+    gdown.download(url, dest, quiet=False)
+
+def unzip_file(file_path: str, extract_path: str):
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+
+
+def get_dataset(url:str, dest:str = "dataset"):
+    folder_path = os.path.join(resources_folder, dest)
+    zip_path = folder_path + ".zip"
+    if os.path.exists(folder_path):
+        print(f"Dataset already dowloaded at {folder_path}")
+        return
+    down_drive(url = url, dest = zip_path)
+    print(f"Extracting to {folder_path} ...")
+    unzip_file(zip_path, resources_folder)
+    os.remove(zip_path)
+    print("Done!")
+
 if __name__ == "__main__":
     download_and_extract_dataset()
+    get_dataset("https://drive.google.com/uc?export=download&id=16cXZ2etHDMrgXSXtLlZ5HuPh7e0KSkVk", "256")
+    get_dataset("https://drive.google.com/uc?export=download&id=19WNetKoHChsVk4KDBAgnhLQEcS2ILgId", "128")
