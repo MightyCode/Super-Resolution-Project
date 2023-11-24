edf8e92df5985b95e27ab98081f72700d7291a91
Marsolo1 <martialmarzloff@gmail.com>
2023-11-24 15:50:50 +0100

    finalized download_resources.py

--- download_resources.py
+++ download_resources.py
@@ -1,38 +1,44 @@
 import os
-import sys
+from typing import Any
 import gdown
 import zipfile
 from sklearn.model_selection import train_test_split
 import json
 from torch.utils.data import Dataset
-from cv2 import imread, imwrite, resize
+import cv2
 
 class CarlaDataset(Dataset):
-    def __init__(self, high_res:str = "1920x1080", low_res:str = "1280x720", split:str = "train", download:bool = False):
+    def __init__(self, high_res:str = "1920x1080", low_res:str = "1280x720", split:str = "train", transforms = None, download:bool = False):
         super().__init__()
         self.split = split
         self.resources_folder: str = "resources"
         self.high_res = high_res
         self.low_res = low_res
+        self.train = "train"
+        self.test = "test"
+        self.transforms = transforms
+        self.dataset_link = self.get_link(high_res)
+
+        if self.look_for_dataset():
+            print("Dataset already present")
+            return
+        if download:
+            self.download_dataset(self.dataset_link, self.high_res)
+    
+        self.resize_dataset(self.high_res, self.low_res)
+        self.split_dataset()
+
+
+    def get_link(self, res:str):
         with open("links.json") as f:
             data = json.load(f)
             try:
-                self.dataset_link = data["datasets"][high_res]
+                return data["datasets"][res]
             except:
-                raise KeyError(f"{high_res} dataset link not found")
-
-        if not download and not self.check_download():
-            raise FileNotFoundError("You didn't download the dataset, you can do it by specifying download=True")
-        else:
-            self.download_dataset(self.dataset_link, self.high_res)
-            self.resize_dataset()
-            # self.split_dataset()
-
-    def check_download(self) -> bool:
-        return os.path.exists(os.path.join(self.resources_folder, self.high_res))
-
-    def down_drive(self, url: str, dest: str):
-        gdown.download(url, dest, quiet=False)
+                raise KeyError(f"{res} dataset link not found")
+            
+    def look_for_dataset(self) -> bool:
+        return os.path.exists(os.path.join(self.resources_folder, self.train)) or os.path.exists(os.path.join(self.resources_folder, self.test))
 
     def unzip_file(self, file_path: str, extract_path: str):
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
@@ -50,74 +56,90 @@ class CarlaDataset(Dataset):
         os.remove(zip_path)
         print("Done!")
 
-    def resize_dataset(self, source:str, dest:str):
+    def resize_dataset(self, source:str, dest:str) -> None:
         source_folder = os.path.join(self.resources_folder, source)
         dest_folder = os.path.join(self.resources_folder, dest)
+        if os.path.exists(dest_folder):
+            print("Dataset already resized")
+            return
+        os.mkdir(dest_folder)
         destx, desty = dest.split("x")
         print("Resizing images ...")
         for img in os.listdir(source_folder):
-            source_img = imread(os.path.join(source_folder, img))
-            dest_img = resize(source_img,(destx, desty))
-            imwrite(os.path.join(dest_folder, img), dest_img)
+            source_img = cv2.imread(os.path.join(source_folder, img))
+            try:
+                dest_img = cv2.resize(source_img,(int(destx), int(desty)))
+                dest_img_path = os.path.join(dest_folder, img)
+                cv2.imwrite(dest_img_path, dest_img)
+            except:
+                print(f"Broken image, skipping it ({img})")
+                os.remove(os.path.join(source_folder, img))
+        assert len(os.listdir(source_folder)) == len(os.listdir(dest_folder))
         print("Done!")
 
+    def split_dataset(self) -> None:
+        dir_train_high = os.path.join(self.resources_folder, self.train, self.high_res)
+        dir_train_low = os.path.join(self.resources_folder, self.train, self.low_res)
+        dir_test_high = os.path.join(self.resources_folder, self.test, self.high_res)
+        dir_test_low = os.path.join(self.resources_folder, self.test, self.low_res)
+        if os.path.exists(dir_train_high):
+            print("Dataset already splitted")
+            return
+        
+        os.makedirs(dir_train_high)
+        os.makedirs(dir_train_low)
+        os.makedirs(dir_test_high)
+        os.makedirs(dir_test_low)
 
-    def remove_folder(self, folder_path: str):
+        dir_high = os.path.join(self.resources_folder, self.high_res)
+        dir_low = os.path.join(self.resources_folder, self.low_res)
+        images = os.listdir(dir_high)
+
+        print("Moving images...")
+        self._move_images(images[:int(0.8*len(images))], dir_high, dir_train_high)
+        self._move_images(images[:int(0.8*len(images))], dir_low, dir_train_low)
+        self._move_images(images[int(0.8*len(images)):], dir_high, dir_test_high)
+        self._move_images(images[int(0.8*len(images)):], dir_low, dir_test_low)
+
+        self._remove_folder(dir_high)
+        self._remove_folder(dir_low)
+        print("Done!")
+
+    def _move_images(self, images:list, source:str, dest:str) -> None:
+        for img in images:
+            source_img = os.path.join(source, img)
+            dest_img = os.path.join(dest, img)
+            os.rename(source_img, dest_img)
+
+    def _remove_folder(self, folder_path: str) -> None:
         try:
             os.rmdir(folder_path)
             print(f"Folder '{folder_path}' removed successfully.")
         except OSError as e:
             print(f"Error: {e}")
 
-    def organize_dataset(self, split: float=0.8):
-        os.makedirs(self.carla_folder, exist_ok=True)  # Create 'carla' folder if it doesn't exist
+    def __len__(self):
+        return len(os.listdir(os.path.join(self.resources_folder, self.split, self.high_res)))
 
-        high_res_url = "https://drive.google.com/uc?export=download&id=16cXZ2etHDMrgXSXtLlZ5HuPh7e0KSkVk"
-        low_res_url = "https://drive.google.com/uc?export=download&id=19WNetKoHChsVk4KDBAgnhLQEcS2ILgId"
+    def __getitem__(self, index) -> Any:
+        dir_path = os.path.join(self.resources_folder, self.split)
+        high_res_path = os.path.join(dir_path, self.high_res)
+        low_res_path = os.path.join(dir_path, self.low_res)
+        images = os.listdir(high_res_path)
+        high_res = cv2.imread(os.path.join(high_res_path, images[index]))
+        low_res = cv2.imread(os.path.join(low_res_path, images[index]))
+        if self.transforms is not None:
+            high_res = self.transforms(high_res)
+            low_res = self.transforms(low_res)
+        
+        return low_res, high_res
 
-        high_res_dest = "256.zip"
-        low_res_dest = "128.zip"
-
-        self.get_dataset(high_res_url, high_res_dest)
-        self.get_dataset(low_res_url, low_res_dest)
-
-        self.split_dataset(split)
-
-        self.remove_folder(os.path.join(self.carla_folder, os.path.splitext(high_res_dest)[0]))
-        self.remove_folder(os.path.join(self.carla_folder, os.path.splitext(low_res_dest)[0]))
-
-    def split_dataset(self, split: float):
-        carla_train_folder = os.path.join(self.carla_folder, "train")
-        carla_valid_folder = os.path.join(self.carla_folder, "valid")
-
-        os.makedirs(carla_train_folder, exist_ok=True)
-        os.makedirs(carla_valid_folder, exist_ok=True)
-
-        high_res_folder = os.path.join(self.carla_folder, "256")
-        low_res_folder = os.path.join(self.carla_folder, "128")
-
-        high_res_images = os.listdir(high_res_folder)
-        low_res_images = os.listdir(low_res_folder)
-
-        high_res_train, high_res_valid = train_test_split(high_res_images, train_size=split, random_state=42)
-        low_res_train, low_res_valid = train_test_split(low_res_images, train_size=split, random_state=42)
-
-        self.organize_images(high_res_folder, high_res_train, carla_train_folder, "high_res")
-        self.organize_images(low_res_folder, low_res_train, carla_train_folder, "low_res")
-
-        self.organize_images(high_res_folder, high_res_valid, carla_valid_folder, "high_res")
-        self.organize_images(low_res_folder, low_res_valid, carla_valid_folder, "low_res")
-
-    def organize_images(self, source_folder: str, image_list: list, destination_folder: str, subfolder: str):
-        subfolder_path = os.path.join(destination_folder, subfolder)
-        os.makedirs(subfolder_path, exist_ok=True)
-
-        for image in image_list:
-            source_path = os.path.join(source_folder, image)
-            destination_path = os.path.join(subfolder_path, image)
-            os.rename(source_path, destination_path)
+    @staticmethod
+    def clean_dir(dir:str = "resources"):
+        for d in os.listdir(dir):
+            if os.path.isdir(d):
+                os.rmdir(d)
 
 if __name__ == "__main__":
-    # split = 0.8 if len(sys.argv) < 2 else float(sys.argv[1])
-    test = CarlaDataset("256x256", "128x128", download=True)
-    # test.organize_dataset(split)
+    # CarlaDataset.clean_dir("resources")
+    test = CarlaDataset("1920x1080", "1280x720", download=True)
