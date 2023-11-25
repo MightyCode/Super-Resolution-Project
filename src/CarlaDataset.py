102908c7a33336ae504744895d8c95cda9c87e45
ybelkhedra <yamine.belkhedra@enseirb-matmeca.fr>
2023-11-25 16:02:19 +0100

    adding dataset in the size of 16x16

--- src/CarlaDataset.py
+++ src/CarlaDataset.py
@@ -139,7 +139,97 @@ class CarlaDataset(Dataset):
         for d in os.listdir(dir):
             if os.path.isdir(d):
                 os.rmdir(d)
+    
+    def get_info(self):
+        # Display the sizes of the dataset
+        dir_train_high = os.path.join(self.resources_folder, self.train, self.high_res)
+        dir_train_low = os.path.join(self.resources_folder, self.train, self.low_res)
+        dir_test_high = os.path.join(self.resources_folder, self.test, self.high_res)
+        dir_test_low = os.path.join(self.resources_folder, self.test, self.low_res)
+
+        print(f'Number of train low resolution images: {len(os.listdir(dir_train_low))}')
+        print(f'Number of train high resolution images: {len(os.listdir(dir_train_high))}')
+        print(f'Number of valid low resolution images: {len(os.listdir(dir_test_low))}')
+        print(f'Number of valid high resolution images: {len(os.listdir(dir_test_high))}')
+
+
+
+class CarlaDataset16x16(CarlaDataset):
+    def __init__(self, high_res:str = "1920x1080", low_res:str = "1280x720", split:str = "train", transforms = None, download:bool = False):
+        super().__init__(high_res, low_res, split, transforms, download)
+        #open the first image of the train low res set to get the size of the images
+        I = cv2.imread(os.path.join(self.resources_folder, self.train, self.low_res, os.listdir(os.path.join(self.resources_folder, self.train, self.low_res))[0]))
+        self.h = I.shape[0] // 16
+        self.w = I.shape[1] // 16
+        self.scale_factor = int(self.high_res.split("x")[0]) // int(self.low_res.split("x")[0])
+    
+    def __getitem__(self, index) -> Any:
+        h = self.h
+        w = self.w
+        index = index // (h * w)
+
+        dir_path = os.path.join(self.resources_folder, self.split)
+        high_res_path = os.path.join(dir_path, self.high_res)
+        low_res_path = os.path.join(dir_path, self.low_res)
+        images = os.listdir(high_res_path)
+
+        image_low_res = cv2.imread(os.path.join(low_res_path, images[index]))
+        image_high_res = cv2.imread(os.path.join(high_res_path, images[index]))
+
+        if self.transforms is not None:
+            image_low_res = self.transforms(image_low_res)
+            image_high_res = self.transforms(image_high_res)
+
+        #take a 16x16 patch corresponding to index for the low res image and the 32x32 patch for the high res image
+        i = index % (h * w)
+        line = i // w
+        col = i % h
+        sf = self.scale_factor
+        image_low_res = image_low_res[:, 16*line:16*(line+1), 16*col:16*(col+1)]
+        image_high_res = image_high_res[:, 16*sf*line:16*sf*(line+1), 16*sf*col:16*sf*(col+1)]
+
+        return image_low_res, image_high_res
+    
+
+    def get_info(self):
+        # Display the sizes of the dataset
+        dir_train_high = os.path.join(self.resources_folder, self.train, self.high_res)
+        dir_train_low = os.path.join(self.resources_folder, self.train, self.low_res)
+        dir_test_high = os.path.join(self.resources_folder, self.test, self.high_res)
+        dir_test_low = os.path.join(self.resources_folder, self.test, self.low_res)
+
+        print(f'Number of train low resolution images: {len(os.listdir(dir_train_low)) * self.h * self.w}')
+        print(f'Number of train high resolution images: {len(os.listdir(dir_train_high)) * self.h * self.w}')
+        print(f'Number of valid low resolution images: {len(os.listdir(dir_test_low)) * self.h * self.w}')
+        print(f'Number of valid high resolution images: {len(os.listdir(dir_test_high)) * self.h * self.w}')
 
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import torchvision
+    import numpy as np
     # CarlaDataset.clean_dir("resources")
-    test = CarlaDataset("1920x1080", "1280x720", download=True)
+    test = CarlaDataset("1920x1080", "960x540", transforms = torchvision.transforms.ToTensor(), download=False)
+    test.get_info()
+    print(test[0][0].shape)
+    print(test[0][1].shape)
+    test.get_info()
+    low_res, high_res = test[0]
+    plt.imshow(low_res.permute(1, 2, 0))
+    plt.show()
+    plt.imshow(high_res.permute(1, 2, 0))
+    plt.show()
+
+
+    lr = CarlaDataset16x16(high_res="1920x1080", low_res="960x540", split="train", transforms = torchvision.transforms.ToTensor(), download=False)
+    print(lr[0][0].shape)
+    print(lr[0][1].shape)
+    lr.get_info()
+    for i in range(3):
+        low_res, high_res = lr[np.random.randint(len(lr))]
+        #fig to show the 16x16 patch and the 32x32 patch
+        fig = plt.figure()
+        fig.add_subplot(1, 2, 1)
+        plt.imshow(low_res.permute(1, 2, 0))
+        fig.add_subplot(1, 2, 2)
+        plt.imshow(high_res.permute(1, 2, 0))
+        plt.show()
