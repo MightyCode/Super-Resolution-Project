eb9cbdb6a0c7ccc289623a3bb02ee1f19100c34e
ybelkhedra <yamine.belkhedra@enseirb-matmeca.fr>
2024-01-28 13:09:23 +0100

    tried to fix app.py and script to upscale video started

--- app.py
+++ app.py
@@ -20,14 +20,18 @@ def get_image():
     return uploaded_img
 
 # Function to make predictions
-def predict(model, lr_data_numpy, device) -> np.ndarray:
+def predict(mod, lr_data_numpy, device) -> np.ndarray:
     # Preprocess image by reversing the channels and applying the transform
     lr_data_torch = common_transform(lr_data_numpy).to(device)
 
-    print(lr_data_torch.shape, type(lr_data_torch))
-    pred_img_tensor = model(lr_data_torch)
+    # lr_img_tensor = torchUtil.filter_data_to_img(lr_data_torch)
 
-    return torchUtil.tensor_to_numpy(pred_img_tensor)
+    pred_img_torch = mod(lr_data_torch.unsqueeze(0)).squeeze(0)
+
+    pred_img_np = torchUtil.tensor_to_numpy(pred_img_torch)
+    pred_img = torchUtil.numpy_to_image(pred_img_np)
+
+    return pred_img
 
 """ 
 Create an argument parser :
@@ -37,9 +41,9 @@ Create an argument parser :
 """
 def get_arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", help="path to the model", type=str)
-    parser.add_argument("-c", "--channel", help="channel used by the model", type=str)
-    parser.add_argument("-u", "--upscale", help="upscale factor", type=int)
+    parser.add_argument("-p", "--path", help="path to the model", type=str, default="weights-upscale-residual-lpis-v.2")
+    parser.add_argument("-c", "--channel", help="channel used by the model", type=str, default="bgr")
+    parser.add_argument("-u", "--upscale", help="upscale factor", type=int, default=2)
 
     return parser
 
@@ -49,11 +53,11 @@ if __name__ == "__main__":
 
     RESULT_PATH = "results/"
 
-    PATH = "weights-upscale-residual-lpis-v.2" if args.path is None else args.path
+    PATH = args.path
     PATH = RESULT_PATH + PATH    
 
-    CHANNELS = "bgr" if args.channel is None else args.channel
-    UPSCALE_FACTOR = 2 if args.upscale is None else args.upscale
+    CHANNELS = args.channel
+    UPSCALE_FACTOR = args.upscale
 
     CHANNEL_INTERPOLATIONS = {
         "b" : "bicubic",
@@ -63,11 +67,8 @@ if __name__ == "__main__":
         "s" : "nearest",
     }
 
-    (f"Usage : streamlit run app.py <model_name> <model_path>")
-    print(f"Default is :", PATH)
-
     device = "cuda" if torch.cuda.is_available() else "cpu"
-
+    
     mod = InitModel.create_model(
         PATH, 
         {
@@ -77,20 +78,23 @@ if __name__ == "__main__":
         }, 
         UPSCALE_FACTOR, device)
 
+
     # Streamlit app
     st.title("Image Super resolution")
     img = get_image()
     # Display the uploaded image
     if img is not None:
         img = Image.open(img)
+        #save the image in the results folder
+        img.save("results/image_tmp.png")
         img = np.array(img)
         lr_data_numpy = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
         st.image(torchUtil.numpy_to_image(lr_data_numpy), caption="Uploaded Image.", use_column_width=False)
-            
-        st.subheader(str(lr_data_numpy.shape))
-        pred_img_numpy = predict(mod, lr_data_numpy, device)
+
+        lr_data_np = torchUtil.open_data("results/image_tmp.png")
+        pred_img = predict(mod, lr_data_np, device)
 
         st.subheader("Result:")
-        st.image(torchUtil.numpy_to_image(pred_img_numpy), caption="Super resolution Image.", use_column_width=False)
+        st.image(pred_img, caption="Super resolution Image.", use_column_width=False)
 
---
+++ upscale_video.py
@@ -0,0 +1,106 @@
+import numpy as np
+import argparse
+import torch
+import os
+import cv2
+from PIL import Image
+from src.utils.PytorchUtil import PytorchUtil as torchUtil
+from torchvision import transforms
+from src.models.InitModel import InitModel
+
+common_transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+def upsale_video(video_path, model, video_out, show):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print("Erreur lors de l'ouverture de la vidéo.")
+        exit()
+
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    fps = cap.get(5)
+
+    out = cv2.VideoWriter(video_out, cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
+
+    while True:
+        ret, frame = cap.read()
+
+        if show:
+            cv2.imshow('frame', frame)
+
+        if not ret:
+            break
+
+        frame = (frame / 255.0).astype(np.float32)
+        lr_data_numpy = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        lr_data_numpy = frame
+
+        lr_data_torch = common_transform(lr_data_numpy).to(device)
+
+        pred_img_tensor = model(lr_data_torch.unsqueeze(0)).squeeze(0)
+
+        output_frame = torchUtil.tensor_to_numpy(pred_img_tensor)
+
+        output_frame = (output_frame * 255).astype(np.uint8)
+
+        if show:
+            cv2.imshow('Sortie du modele', output_frame)
+        
+        out.write(output_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", help="path to the model", type=str, default="weights-upscale-residual-lpis-v.2")
+    parser.add_argument("-c", "--channel", help="channel used by the model", type=str, default="bgr")
+    parser.add_argument("-u", "--upscale", help="upscale factor", type=int, default=2)
+    parser.add_argument("-v", "--video", help="upscale video", type=str, default="resources/video.mp4")
+    parser.add_argument("-o", "--output", help="upscale video", type=str, default='results/video_out.mp4')
+    parser.add_argument("-s", "--show", help="show video", type=bool, default=False)
+
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_arg_parser()
+    args = parser.parse_args()
+
+    RESULT_PATH = "results/"
+
+    PATH = args.path
+    PATH = RESULT_PATH + PATH    
+
+    CHANNELS = args.channel
+    UPSCALE_FACTOR = args.upscale
+
+    CHANNEL_INTERPOLATIONS = {
+        "b" : "bicubic",
+        "g" : "bicubic",
+        "r" : "bicubic",
+        "d" : "nearest",
+        "s" : "nearest",
+    }
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    model = InitModel.create_model(
+        PATH, 
+        {
+            "learningRate": 0.001,
+            "channel_positions" : CHANNELS,
+            "channel_interpolations" : [CHANNEL_INTERPOLATIONS[c] for c in CHANNELS],
+        }, 
+        UPSCALE_FACTOR, device)
+
+    upsale_video(args.video, model, args.output, args.show)
